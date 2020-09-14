37c2f6369ab2c3094bd528a26d0a9d10ec7999b0
SaiLikhith Kanuparthi <slk522@nyu.edu>
2020-09-14 14:53:40 -0400

    summary

---
+++ build/precache-manifest.ba13570385ff79efdd293efc18b6ce73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6deec6c4aeb893131ee76fa313c7c0da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cc7e9f443f7c2f64cb9",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "a3f8b6eb3da752bf821b",
+    "url": "/static/js/2.cfb8e101.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.cfb8e101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc7e9f443f7c2f64cb9",
+    "url": "/static/js/main.3b03ca06.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
